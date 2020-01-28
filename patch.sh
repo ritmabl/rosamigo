2d1390be547b5a3d0ced9094f4e9c4c6ba5a1d2d
Anmol Modur <asm7148@g.rit.edu>
2020-01-28 15:36:53 -0500

    Fixed CMakeLists.txt to  add libraries

--- patch.sh
+++ patch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Setting up permissions..."
-chmod +x $HOME/$YOUR_WS/src/rosaria/cfg/RosAria.cfg
-chmod +x $HOME/$YOUR_WS/src/mypkg/my_publisher.py
-chmod +x $HOME/$YOUR_WS/src/mypkg/my_subscriber.py
+sudo chmod +x $HOME/$YOUR_WS/src/rosaria/cfg/RosAria.cfg
+sudo chmod +x $HOME/$YOUR_WS/src/mypkg/my_publisher.py
+sudo chmod +x $HOME/$YOUR_WS/src/mypkg/my_subscriber.py
 echo "Setup complete.."
